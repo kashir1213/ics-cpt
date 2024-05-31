e5350752e2c01c6d597d9100d109e8c8926a94f1
kashir1213 <muhammadkashir2007@gmail.com>
2024-05-31 10:02:36 -0400

    additional stuff

--- game_levels/asteroids.py
+++ game_levels/asteroids.py
@@ -9,18 +9,29 @@ RED = (255, 0, 0)
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 
-asteroidImage  = pygame.image.load('C:/Users/Kashir/OneDrive - Dufferin-Peel Catholic District School Board/ICS3UC/CPT/asteroid.png')
+# asteroidImage  = pygame.image.load('C:/Users/Kashir/OneDrive - Dufferin-Peel Catholic District School Board/ICS3UC/CPT/asteroid.png')
 
 class makeAsteroid():
     def __init__(self):
+        self.image = pygame.image.load('C:/Users/Kashir/OneDrive - Dufferin-Peel Catholic District School Board/ICS3UC/CPT/asteroid.png')
         self.width = random.randint(100, 500)
         self.height = random.randint(100, 500)
-        
 
-    
-    def draw(self, image):
-        asteroidImage = pygame.transform.scale(image, (self.width,self.height))
-        screen.blit(asteroidImage, (50,50))
+        self.posX = 0
+        self.posY = 0
+
+        self.moveX = random.randint(0,600) 
+        self.moveY = random.randint(500,700)       
+
+    def move(self):
+        while self.posX == self.moveX:
+            self.posX += 10
+        while self.posY == self.moveY:
+            self.posY += 10
+        
+    def draw(self):
+        asteroidImage = pygame.transform.scale(self.image, (self.width,self.height))
+        screen.blit(asteroidImage, (self.posX,self.posY))
 
 
 pygame.init()
@@ -37,32 +48,33 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-# while not done:
-#     # --- Main event loop
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             done = True
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
  
-#     # --- Game logic should go here
+    # --- Game logic should go here
  
-#     # --- Screen-clearing code goes here
+    # --- Screen-clearing code goes here
     
     
-#     # Here, we clear the screen to white. Don't put other drawing commands
-#     # above this, or they will be erased with this command.
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
  
-#     # If you want a background image, replace this clear with blit'ing the
-#     # background image.
-#     screen.fill(BLACK)
-#     x.draw(asteroidImage)
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(BLACK)
+    x.draw()
+    x.move()
  
-#     # --- Drawing code should go here
+    # --- Drawing code should go here
  
-#     # --- Go ahead and update the screen with what we've drawn.
-#     pygame.display.flip()
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
  
-#     # --- Limit to 60 frames per second
-#     clock.tick(60)
+    # --- Limit to 60 frames per second
+    clock.tick(60)
  
-# # Close the window and quit.
-# pygame.quit()
+# Close the window and quit.
+pygame.quit()
--- game_levels/boxes.py
+++ game_levels/boxes.py
@@ -45,26 +45,26 @@ YELLOW = (255,255,0)
 DARKGREEN = (0,51,0)
 
 
-
+#summon boxes at random locations
     
 
-## Main Program Loop
-# while not done:
-#     ## CONTROL
-#     # Check for events
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             done = True
+# Main Program Loop
+while not done:
+    ## CONTROL
+    # Check for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
 
-#     screen.fill(BLACK)
-#     summon = random.randint(0,10)
-#     #need to change
-#     theboxes = boxes()
-#     if summon == 5:
-#         theboxes.drawBox()
+    screen.fill(BLACK)
+    summon = random.randint(0,10)
+    #need to change
+    theboxes = boxes()
+    if summon == 5:
+        theboxes.drawBox()
         
-#     # Update Screen
-#     pygame.display.flip()
-#     clock.tick(60)
-# # Close the window and quit
-# pygame.quit()
+    # Update Screen
+    pygame.display.flip()
+    clock.tick(60)
+# Close the window and quit
+pygame.quit()
